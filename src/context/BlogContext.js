711823260f400cb2edd5040cf303e02182520362
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-02 11:38:38 +0200

    completed lesson 127: render list

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,13 @@ import React from 'react';
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({ children }) => {
+    const blogPosts = [
+        { title: 'Blog post #1' },
+        { title: 'Blog post #2' }
+    ]
+
     return (
-        <BlogContext.Provider value={15}>
+        <BlogContext.Provider value={blogPosts}>
             {children}
         </BlogContext.Provider>
     )
