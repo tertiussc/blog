e85d8f218902ce24d9a452ebd0cb787697bba16f
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-08 11:40:17 +0200

    completed lesson 136: Added delete blogpost

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext'
 import { FontAwesome } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context)
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
     return (
         <View>
@@ -15,8 +15,10 @@ const IndexScreen = () => {
                 renderItem={({ item }) => {
                     return (
                         <View style={styles.row}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <FontAwesome name="trash-o" style={styles.icon} />
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <FontAwesome name="trash-o" style={styles.icon} />
+                            </TouchableOpacity>
                         </View>
                     )
                 }}
