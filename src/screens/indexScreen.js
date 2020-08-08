af37b1d5f570b6aed53b776c451bb84852a9fb88
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-08 11:20:30 +0200

    completed lesson 134: Added styling

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,19 +1,24 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext'
+import { FontAwesome } from '@expo/vector-icons';
 
 const IndexScreen = () => {
     const { state, addBlogPost } = useContext(Context)
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost} />
             <FlatList
                 data={state}
                 keyExtractor={(blogPosts) => blogPosts.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <FontAwesome name="trash-o" style={styles.icon} />
+                        </View>
+                    )
                 }}
             />
         </View>
@@ -21,7 +26,20 @@ const IndexScreen = () => {
 }
 
 const styles = StyleSheet.create({
-
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderColor: 'grey'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
 })
 
 export default IndexScreen;
